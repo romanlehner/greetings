3dc1ee947058095f11ba181bab914d3b966c41b8
Roman Lehner <romanlehner@airasia.com>
2020-09-07 14:36:41 +0800

    add unit test

---
+++ greetings_test.go
@@ -0,0 +1,10 @@
+package greetings
+
+import "testing"
+
+func TestHelloWorld(t *testing.T) {
+	want := "Hello World"
+	if got := HelloWorld(); got != want {
+		t.Errorf("HelloWorld() = %q, want %q", got, want)
+	}
+}
