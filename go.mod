14469ce9d4fa1b7b129e94ae044e124c2345b599
Roman Lehner <romanlehner@airasia.com>
2020-09-08 10:44:50 +0800

    test versioning recreate old version

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/romanlehner/greetings
+
+go 1.15
---
+++ greetings.go
@@ -0,0 +1,8 @@
+package greetings
+
+import "fmt"
+
+// HelloWorld returns a Hello World string in english language
+func HelloWorld() {
+	fmt.Println("Hello World!")
+}
